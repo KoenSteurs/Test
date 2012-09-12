766ccca18b59942b2494528e40c9736898fd531e
Koen Steurs <ksteurs@logsys.eu>
2012-09-12 11:42:17 +0200

    modified web.config

--- TestApp/TestApp/Controllers/DriverController.cs
+++ TestApp/TestApp/Controllers/DriverController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Web;   
 using System.Web.Mvc;
 using KartingApp.Domain.Abstract;
 using KartingApp.Domain.Entities;
